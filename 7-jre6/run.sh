2345eea3832806d85ccbd91b445e1a0d92761fdb
Stéphane Cottin <stephane.cottin@vixns.com>
2015-04-24 18:29:17 +0200

    deploy on ROOT if APP_NAME not provided

--- 7-jre6/run.sh
+++ 7-jre6/run.sh
@@ -4,5 +4,11 @@ if [ -z "${WAR_URL}" ]; then
   echo "WAR_URL env var required" > /dev/stderr
   exit 1
 fi
-curl -s -k -o /usr/local/tomcat/webapps/${APP_NAME:-app}.war "${WAR_URL}"
+
+curl -s -k -o /usr/local/tomcat/webapps/${APP_NAME:-ROOT}.war "${WAR_URL}"
+
+if [ -z "${APP_NAME}"]; then
+	rm -rf /usr/local/tomcat/webapps/ROOT
+fi
+
 exec catalina.sh run
